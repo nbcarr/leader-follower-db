c1c6b81c9b7c0c58eebb2a9c348a115695d95827
nbcarr <ddd@Nicks-MacBook-Pro.local>
2025-02-05 15:17:31 -0500

    fix start.sh script

--- start.sh
+++ start.sh
@@ -4,5 +4,5 @@ python3 db.py --role leader --port 8000 --peers 8001 8002 &
 sleep 1
 python3 db.py --role follower --port 8001 --peers 8000 8002 &
 sleep 1
-python3 db.py --role follower --port 8002 --peers 8001 8002 &
+python3 db.py --role follower --port 8002 --peers 8000 8001 &
 wait
